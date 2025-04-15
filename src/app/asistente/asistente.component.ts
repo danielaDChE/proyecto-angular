e8afd18e5d8c98314cb494f6e69560f1149d894d
xiac <x.i.a.c.1504@gmail.com>
2025-04-14 20:51:19 -0400

    conexion con indexd y creacion de la tabla Asistente

--- src/app/asistente/asistente.component.ts
+++ src/app/asistente/asistente.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AsistenteService } from '../servicios/asistente.service';
 
 @Component({
   selector: 'es-asistente',
@@ -11,10 +12,29 @@ import { FormsModule } from '@angular/forms';
 export class AsistenteComponent {
   asistente = signal({
     nombre : '',
-    edad : ''
+    edad : null
   });
+  asistentes = signal<any[]>([]);
+
+  constructor(private dbServicio : AsistenteService) {
+    this.cargarAsistentes();
+    
+  }
 
   registrarAsistencia() {
-    console.log('asistente registrado', this.asistente());
+    const datos = this.asistente(); 
+    if (datos.nombre && datos.edad !== null) {
+      this.dbServicio.guardarAsistente({nombre : datos.nombre, edad : datos.edad})
+        .then(() => {
+            this.asistente.set({ nombre : '' , edad : null});
+            this.cargarAsistentes();
+            // console.log("registro correctamente ------");
+        }).catch(() => console.log("error....."));
+    }
+  }
+
+  cargarAsistentes() {
+    this.dbServicio.obtenerAsistentes()
+      .then((lista) => { this.asistentes.set(lista) });
   }
 }
--- src/app/servicios/asistente.service.ts
+++ src/app/servicios/asistente.service.ts
@@ -4,6 +4,51 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AsistenteService {
+  private dbNombre = 'EscuelaSabaticaDB';
+  private objetoAsistente = 'asistentes';
+  private db! : IDBDatabase;
+  constructor() { 
+    this.initDB();
+  }
 
-  constructor() { }
+  initDB (){
+    const request = indexedDB.open(this.dbNombre, 1);   
+    request.onsuccess = () => {
+        this.db = request.result;
+        console.log("Base de datos abierta", this.db);
+    }
+
+    request.onupgradeneeded = (event : any) => {
+        this.db = event.target.result;
+        if(!this.db.objectStoreNames.contains(this.objetoAsistente)){
+          this.db.createObjectStore(this.objetoAsistente, {keyPath: 'id', autoIncrement: true });
+        }
+      
+    }
+
+    request.onerror = (error) =>{
+        console.log('Error al abrir la DB',error);
+    }      
+  }
+ 
+  guardarAsistente( asistente : {nombre : string; edad: string}) : Promise<void>{
+    return new Promise((satisfactorio, rechazo) => {
+      const trs =  this.db.transaction([this.objetoAsistente], 'readwrite');
+      const objetoAsistente = trs.objectStore(this.objetoAsistente);
+      objetoAsistente.add(asistente);
+      trs.oncomplete = () => satisfactorio();
+      trs.onerror = () => rechazo(trs.error);
+
+    });
+  }
+
+  obtenerAsistentes () : Promise<any[]>{
+    return new Promise((satisfactorio, rechazo) => {
+      const trs =  this.db.transaction([this.objetoAsistente], 'readonly');
+      const objetoAsistente = trs.objectStore(this.objetoAsistente);
+      const request =  objetoAsistente.getAll();
+      request.onsuccess = () => satisfactorio(request.result);
+      request.onerror = () => console.log("existe un error");
+    });
+  }
 }
